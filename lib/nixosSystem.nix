d43292e004a0b23c30b738923fd2d0014e3776d6
Jun-Hong Cheng <qwe854896@gmail.com>
2024-08-12 03:44:25 +0800

    feat: add module wrapper for nixos

---
+++ lib/nixosSystem.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  inputs,
+  nixos-modules,
+  home-modules ? [],
+  myvars,
+  system,
+  genSpecialArgs,
+  specialArgs ? (genSpecialArgs system),
+  ...
+}: let
+  inherit (inputs) nixpkgs home-manager nixos-generators;
+in
+  nixpkgs.lib.nixosSystem {
+    inherit system specialArgs;
+    modules =
+      nixos-modules
+      ++ [
+        nixos-generators.nixosModules.all-formats
+      ]
+      ++ (
+        lib.optionals ((lib.lists.length home-modules) > 0)
+        [
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.extraSpecialArgs = specialArgs;
+            home-manager.users."${myvars.username}".imports = home-modules;
+          }
+        ]
+      );
+  }

35d4f7e2e563119a35933bdba32ff08aeb1bfa55
Jun-Hong Cheng <qwe854896@gmail.com>
2024-08-14 16:01:42 +0800

    fix: add workaround for rdp cursor

--- modules/nixos/desktop/rdp/default.nix
+++ modules/nixos/desktop/rdp/default.nix
@@ -10,4 +10,8 @@
     moonlight-qt # moonlight client, for streaming games/desktop from a PC
     rustdesk # p2p remote desktop
   ];
+
+  # An workaround to use cursor in RDP
+  # https://github.com/LizardByte/Sunshine/issues/93
+  environment.variables.KWIN_FORCE_SW_CURSOR = 1;
 }

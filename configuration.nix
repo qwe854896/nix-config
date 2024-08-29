98b0609b9b1c7a6fdc97b7be6e7c659b894c953d
Jun-Hong Cheng <qwe854896@gmail.com>
2024-08-30 01:17:25 +0800

    feat: add git, neovim, and curl

--- configuration.nix
+++ configuration.nix
@@ -97,8 +97,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    git
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    curl
   ];
 
   # Some programs need SUID wrappers, can be configured further or are

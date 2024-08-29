438b249251d15ee4b03cbc28c31349ad7d37fe8d
Jun-Hong Cheng <qwe854896@gmail.com>
2024-08-30 01:37:53 +0800

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "flake for nixos";
+
+  inputs = {
+    # Official NixOS package sources, using unstable branch by default
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
+

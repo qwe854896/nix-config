d07358e3b613b554c39cb7cc41dbaeaf0f2bb2e1
Jun-Hong Cheng <qwe854896@gmail.com>
2024-09-14 22:16:08 +0800

    fix: flameshot bug

--- home/gui-apps.nix
+++ home/gui-apps.nix
@@ -20,4 +20,12 @@
       hwdec = "auto-safe";
     };
   };
+
+  # https://github.com/nix-community/home-manager/issues/2064
+  systemd.user.targets.tray = {
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = ["graphical-session-pre.target"];
+    };
+  };
 }

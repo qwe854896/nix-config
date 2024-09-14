766a4adbbc5f24b48f141ddafcdfebcdce9caf67
Jun-Hong Cheng <qwe854896@gmail.com>
2024-09-14 18:05:01 +0800

    fix: disable packages drop logs

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -5,8 +5,6 @@
   ...
 }: {
   networking.firewall = {
-    # if packets are still dropped, they will show up in dmesg
-    logReversePathDrops = true;
     # wireguard trips rpfilter up
     extraCommands = ''
       ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN

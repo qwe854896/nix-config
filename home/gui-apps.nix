77c8ab9cfb95864ff0f2467db2870551595c30f1
Jun-Hong Cheng <qwe854896@gmail.com>
2024-09-19 15:26:23 +0800

    fix: use normal vscode

--- home/gui-apps.nix
+++ home/gui-apps.nix
@@ -3,9 +3,6 @@
   lib,
   ...
 }: let
-  shellAliases = {
-    "code" = "code-insiders --";
-  };
 in {
   # Browsers
   programs.firefox.enable = true;
@@ -25,30 +22,18 @@ in {
   };
 
   # IDE
-  home.shellAliases = shellAliases;
   programs.vscode = {
     enable = true;
-    package =
-      (pkgs.vscode.override {
-        isInsiders = true;
-
-        # https://wiki.archlinux.org/title/Wayland#Electron
-        commandLineArgs = [
-          # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
-          # (only supported by chromium/chrome at this time, not electron)
-          "--gtk-version=4"
-          # make it use text-input-v1, which works for kwin 5.27 and weston
-          "--enable-wayland-ime"
-        ];
-      })
-      .overrideAttrs (oldAttrs: rec {
-        version = "latest";
-        # Use VSCode Insiders to fix crash: https://github.com/NixOS/nixpkgs/issues/246509
-        src = builtins.fetchTarball {
-          url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-          sha256 = "1v250i7jipb2p5311y6q1ldnp58d2c5nifdl1c5bhidlkjcl651a";
-        };
-      });
+    package = pkgs.vscode.override {
+      # https://wiki.archlinux.org/title/Wayland#Electron
+      commandLineArgs = [
+        # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
+        # (only supported by chromium/chrome at this time, not electron)
+        "--gtk-version=4"
+        # make it use text-input-v1, which works for kwin 5.27 and weston
+        "--enable-wayland-ime"
+      ];
+    };
   };
 
   # Media Player

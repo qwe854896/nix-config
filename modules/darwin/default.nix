f2e6bf2c6e0aab2c27ab7b2daae2a7ca75626d24
Jun-Hong Cheng <qwe854896@gmail.com>
2025-01-02 03:11:53 +0800

    refactor: move homebrew part

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   imports = [
     ./settings.nix
+    ./homebrew.nix
   ];
 
   # https://github.com/LnL7/nix-darwin/wiki/Terminfo-issues
--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -16,35 +16,6 @@ in {
     shell = pkgs.zsh;
   };
 
-  homebrew = {
-    enable = true;
-    taps = builtins.attrNames config.nix-homebrew.taps;
-    brews = pkgs.callPackage ./brews.nix {};
-    casks = pkgs.callPackage ./casks.nix {};
-    onActivation = {
-      autoUpdate = true;
-      cleanup = "zap";
-      upgrade = true;
-    };
-
-    # These app IDs are from using the mas CLI app
-    # mas = mac app store
-    # https://github.com/mas-cli/mas
-    #
-    # $ nix shell nixpkgs#mas
-    # $ mas search <app name>
-    #
-    # If you have previously added these apps to your Mac App Store profile (but not installed them on this system),
-    # you may receive an error message "Redownload Unavailable with This Apple ID".
-    # This message is safe to ignore. (https://github.com/dustinlyons/nixos-config/issues/83)
-
-    masApps = {
-      "RunCat" = 1429033973;
-      "wireguard" = 1451685025;
-      "Messenger" = 1480068668;
-    };
-  };
-
   # Enable home-manager
   home-manager = {
     useGlobalPkgs = true;

8fa46401f4a14f2c99e24f1ae3fb22d16ae3d9e5
Jun-Hong Cheng <qwe854896@gmail.com>
2025-01-02 14:33:53 +0800

    refactor: aggregate small system config

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -1,7 +1,15 @@
 _: {
   imports = [
     ./nix.nix
-    ./gpg.nix
-    ./programs.nix
   ];
+  programs = {
+    # https://discourse.nixos.org/t/fish-shell-plugins-missing-from-profile-on-one-machine-but-not-on-another/21636
+    zsh.enable = true;
+    fish.enable = true;
+
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = false;
+    };
+  };
 }

78defe4fe9baefca7228c7a8f6b7ece273e8b89d
Jun-Hong Cheng <qwe854896@gmail.com>
2025-01-03 16:02:50 +0800

    feat: re-enable dockerCompat for podman

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   virtualisation.podman = {
     enable = true;
-    dockerCompat = false;
+    dockerCompat = true;
     # Required for containers under podman-compose to be able to talk to each other.
     defaultNetwork.settings.dns_enabled = true;
     # Periodically prune Podman resources

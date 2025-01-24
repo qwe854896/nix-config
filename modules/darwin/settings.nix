c2a2335ca603b1da7afc5048ac5862483a8c1586
Jun-Hong Cheng <qwe854896@gmail.com>
2025-01-24 22:30:16 +0800

    fix: disable ApplePressAndHold

--- modules/darwin/settings.nix
+++ modules/darwin/settings.nix
@@ -106,7 +106,7 @@
 
         AppleInterfaceStyle = "Dark";
         AppleKeyboardUIMode = 3; # Mode 3 enables full keyboard control.
-        ApplePressAndHoldEnabled = true;
+        ApplePressAndHoldEnabled = false;
 
         # If you press and hold certain keyboard keys when in a text area, the key’s character begins to repeat.
         # This is very useful for vim users, they use `hjkl` to move cursor.

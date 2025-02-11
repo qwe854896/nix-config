2111ca26a124b3785b554f2aa1f61aef2598a54b
Jun-Hong Cheng <qwe854896@gmail.com>
2025-02-12 01:36:16 +0800

    fix: disable xorg server

--- modules/nixos/gui.nix
+++ modules/nixos/gui.nix
@@ -2,14 +2,17 @@
   programs.dconf.enable = true;
 
   services.displayManager = {
-    sddm.enable = true;
+    sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
     autoLogin.enable = true;
     autoLogin.user = "jhc";
   };
 
   # Configure keymap in X11
   services.xserver = {
-    enable = true;
+    enable = false;
     xkb = {
       layout = "us";
       variant = "";
